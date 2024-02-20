44cb616c687f00891f073262cfa859e66c07b04d
maxwilets <mwilets@gmail.com>
2024-02-20 11:26:42 -0700

    fixing display error [FEAT]

--- src/ProductForm.tsx
+++ src/ProductForm.tsx
@@ -1,36 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Product } from "./types";
 import { withProduct } from "./hooks/withProduct";
 import { ProductBlock } from "./ProductBlock";
 
 export const ProductForm = () => {
   const [formInput, setFormInput] = useState('');
-  const [scrapeData, setScrapeData] = useState<Product[] | []>([])
-  const [dataScraped, setDataScraped] = useState('false')
+  const [scrapeData, setScrapeData] = useState<Product[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [isRequestValid, setIsRequestValid] = useState<boolean | null>(null);
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setFormInput( event.target.value)
-  }
+    setFormInput(event.target.value);
+  };
 
-  const handleSubmit = async() => {
-    setDataScraped('false');
-    const returnData: Product[] = await withProduct(formInput)
-    setScrapeData(returnData)
-    setDataScraped('true')
-  }
+  const handleSubmit = async () => {
+    setIsFetching(true);
+    setIsRequestValid(null); // Reset the validity state when a new request is initiated
+    const returnData: Product[] = await withProduct(formInput);
+    setScrapeData(returnData);
+    setIsFetching(false);
+    setIsRequestValid(returnData.length > 0); // Set the validity state based on the response
+  };
+
+  useEffect(() => {
+    const newInput = scrapeData.filter((item) => item.title != '');
+    setScrapeData(newInput);
+  }, [scrapeData]);
 
   return (
     <div>
       <div className='searchForm'>
-        
-        <input defaultValue='Email' type='email' value={formInput} onChange={ handleInput} />
+        <input defaultValue='Email' type='email' value={formInput} onChange={handleInput} />
         <button onClick={handleSubmit}>Search</button>
       </div>
       <div className="productsContainer">
-        {scrapeData.length === 0 ?  <h4>The Product You Entered Does Not Exist</h4> : scrapeData.map((item, index) => {
-          return index != 0 ? <ProductBlock styleProp={index % 2 === 0? 'light': 'dark'} key={`product-item-${index}`} product={item} />  : null 
+        {isFetching && <h4>Loading...</h4>}
+        {!isFetching && isRequestValid === false && (
+          <h4>The Product You Entered Does Not Exist</h4>
+        )}
+        {!isFetching && isRequestValid !== false && scrapeData.map((item, index) => {
+          return index !== 0 ? (
+            <ProductBlock styleProp={index % 2 === 0 ? 'light' : 'dark'} key={`product-item-${index}`} product={item} />
+          ) : null;
         })}
       </div>
     </div>
-  )
+  );
 }
